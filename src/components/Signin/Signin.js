8ae080e89270cf5c43c92ee76f8d64eb5b527594
moki929 <moking929@gmail.com>
2020-12-20 17:52:26 -0500

    connect to server

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -19,7 +19,7 @@ class Signin extends React.Component {
     };
 
     onSubmitSignIn = () => {
-        fetch("https://facewho.herokuapp.com/signin", {
+        fetch("https://facewho-server.herokuapp.com/signin", {
             method: "post",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({
